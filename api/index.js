1b20d9c23285867e93bd2a60029e3bbc588683b7
MVS Koushik <medurivskoushik@gmail.com>
2026-02-11 14:45:52 +0530

    third commit

--- api/index.js
+++ api/index.js
@@ -5,7 +5,11 @@ const chatRoutes = require("../routes/chat");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173"],
+  methods: ["GET", "POST"],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/chat", chatRoutes);
